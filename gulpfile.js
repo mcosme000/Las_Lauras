f89dafa21810f8aab61214fd70b8580b4b8a24c0
Maria Cosmen <mcosme00@gmail.com>
2021-05-02 00:32:36 +0900

    Added main files and started the header & video

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const { series, src, dest, watch } = require("gulp");
+const sass = require("gulp-sass");
+
+//compile sass function:
+function css() {
+  //looking for the scss file
+  return (
+    src("src/app.scss")
+      //apply the sass function
+      .pipe(sass())
+      //new css file destination
+      .pipe(dest("./build/css"))
+  );
+}
+
+//watchfiles function
+function watchFiles() {
+  watch("src/**/*.scss", css);
+}
+
+exports.css = css;
+
+exports.default = watchFiles;
